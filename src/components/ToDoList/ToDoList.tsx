a8fe664bef03b5444e0851e97830ddd1fb7c2e70
AlexeyZ <azv99@yandex.ru>
2025-10-20 17:17:49 +0300

    3 lesson React+TS

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,24 +1,30 @@
 import './ToDoList.scss'
 import { ToDoListItem } from './ToDoListItem/ToDoListItem'
+import { IToDo } from '../../models/todo-item'
 
-export const ToDoList = () => {
+export const ToDoList = (props: { todos: IToDo[], deleteToDo: Function, updateToDo: Function }) => {
+
+
+  const checkedList = () => {
+    return props.todos
+      .filter(todo => !todo.isDone)
+      .map((todo, idx) => {
+        return <ToDoListItem toDoItem={todo} key={idx} updateToDo={props.updateToDo} deleteToDo={props.deleteToDo}/>
+      })
+  }
+
+  const unCheckedList = () => {
+    return props.todos
+      .filter(todo => todo.isDone)
+      .map((todo, idx) => {
+        return <ToDoListItem toDoItem={todo} key={idx} updateToDo={props.updateToDo} deleteToDo={props.deleteToDo}/>
+      })
+  }
   return (
     <>
       <div className="todo-container">
-        <ul className="todo-list failed">
-          <ToDoListItem />
-        </ul>
-        <ul className="todo-list completed">
-          <ToDoListItem />
-
-          {/* <li className="todo-list-item__wrapper">
-            <span>Вторая задача</span>
-            <div className="todo-list-item__buttons">
-              <button className="btn-trash"></button>
-              <button className="btn-uncheck"></button>
-            </div>
-          </li> */}
-        </ul>
+        <ul className="todo-list failed">{checkedList()}</ul>
+        <ul className="todo-list completed">{unCheckedList()}</ul>
       </div>
     </>
   )
--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,13 +1,49 @@
+import { useState } from 'react'
 import { Form } from '../components/Form/Form'
 import { Header } from '../components/Header/Header'
 import { ToDoList } from '../components/ToDoList/ToDoList'
+import { IToDo } from '../models/todo-item'
+import { ToastContainer, toast } from 'react-toastify'
 
 export const ToDoListPage = () => {
+  const notifyAdd = () => toast.success('Новая задача добавлена')
+  const notifyDelete = () => toast.warning('Задача удалена')
+  const notifyUpdate = () => toast.info('Задача обновлена')
+  const [todos, setTodos] = useState<IToDo[]>([])
+
+  const createNewToDo = (text: string) => {
+    const newToDo: IToDo = {
+      id: todos.length,
+      text: text,
+      isDone: false,
+    }
+    setTodos([...todos, newToDo])
+    notifyAdd()
+  }
+
+  const updateToDo = (todoItem: IToDo) => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === todoItem.id) {
+        todoItem.isDone = !todo.isDone
+      }
+      return todo
+    })
+    setTodos(newTodos)
+    notifyUpdate()
+  }
+
+  const deleteToDo = (todoItem: IToDo) => {
+    const newTodos = todos.filter(todo => todo.id !== todoItem.id)
+    setTodos(newTodos)
+    notifyDelete()
+  }
+
   return (
     <>
       <Header />
-      <Form />
-      <ToDoList />
+      <Form createNewToDo={createNewToDo} />
+      <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
+      <ToastContainer position="bottom-right" theme="colored" autoClose={2000} hideProgressBar={true} />
     </>
   )
 }
